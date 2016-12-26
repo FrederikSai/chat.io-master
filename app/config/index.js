ea9d8975a62bea6ef7ac063294b4755dd3052b15
FrederikSai <saikyawphyowai@gmail.com>
2016-12-26 15:40:01 +0900

    First commit

---
+++ app/config/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var init = function () {
+
+	if(process.env.NODE_ENV === 'production') {
+		var redisURI 		= require('url').parse(process.env.REDIS_URL);
+		var redisPassword 	= redisURI.auth.split(':')[1];
+		return {
+			dbURI: process.env.dbURI,
+			sessionSecret: process.env.sessionSecret,
+			redis: {
+				host: redisURI.hostname,
+				port: redisURI.port,
+				password: redisPassword
+			}
+		}
+	}
+	else {
+		return require('./config.json');
+	}
+}
+
+module.exports = init();
